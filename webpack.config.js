e63ef2dd959ca28cd4d81fabe56edcd7204939b5
Dave <8709533+DaveBathnes@users.noreply.github.com>
2021-08-03 00:00:05 +0100

    #4 Adding node polyfills

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,7 @@
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
+
 module.exports = {
-  resolve: {
-    aliasFields: ['browser', 'browser.esm'],
-    fallback: {
-      buffer: require.resolve("buffer/"),
-      util: require.resolve("util/"),
-      https: require.resolve("https-browserify")
-    }
-  }
+  plugins: [
+    new NodePolyfillPlugin()
+  ]
 }
