82ef82cfce129c50d7482a061a09823e0222a19a
Dave <8709533+DaveBathnes@users.noreply.github.com>
2021-08-03 01:02:41 +0100

    #4 Adding mainFIelds

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,10 @@ const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
 module.exports = {
   mode: 'development',
   entry: path.resolve(__dirname) + '/index.js',
+  resolve: {
+    mainFields: ['browser', 'module', 'main'],
+    aliasFields: ['browser'],
+  },
   plugins: [
     new NodePolyfillPlugin()
   ]
